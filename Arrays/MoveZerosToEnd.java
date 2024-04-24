16144d6202fd22762592a0823add43b277115a57
aravindinduri <thearavindinduri@gmail.com>
2024-04-24 23:50:40 +0530

    added new problem

---
+++ Arrays/MoveZerosToEnd.java
@@ -0,0 +1,63 @@
+
+// BRUTE FORCE 
+// SC - O( n )
+// TC -O( n )
+
+// public class MoveZerosToEnd {
+//     public static void main(String[] args) {
+//         int nums[] = { 1, 0, 2, 3, 0, 4, 0, 1 };
+//         int temp[] = new int[nums.length];
+//         int k =0;
+//         for (int i = 0; i < nums.length; i++) {
+//             if(nums[i] != 0)
+//             {
+//                 temp[k] = nums[i];
+//                 k++;
+//             }
+
+//         }
+//         nums = temp.clone();
+//         for (int i = 0; i < nums.length; i++) {
+//             System.out.println(nums[i]);
+//         }
+
+//     }
+
+// }
+
+// BETTER APPROACH
+// 
+public class MoveZerosToEnd {
+    static void solve(int arr[], int n) {
+        int j = -1;
+        for (int i = 0; i < n; i++) {
+            if (arr[i] == 0) {
+                j = i;
+                break;
+            }
+        }
+        if (j == -1)
+            return;
+
+        for (int i = j + 1; i < n; i++) {
+            if (arr[i] != 0) {
+                int tmp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = tmp;
+                j++;
+            }
+        }
+
+    }
+
+    public static void main(String[] args) {
+        int nums[] = { 1, 0, 2, 3, 0, 4, 0, 1 };
+        int n = nums.length;
+        solve(nums, n);
+        for (int i = 0; i < n; i++) {
+            System.out.println(nums[i]);
+        }
+
+    }
+
+}
