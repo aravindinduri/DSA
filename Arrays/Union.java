6019b6d79637e7f131e93b3dc678d274914559b1
aravindinduri <thearavindinduri@gmail.com>
2024-04-27 23:16:08 +0530

    added changes

--- Arrays/Union.java
+++ Arrays/Union.java
@@ -1,3 +1,4 @@
+
 /*  Union of Two Sorted Arrays
 Problem Statement: Given two sorted arrays, arr1, and arr2 of size n and m. Find the union of two sorted arrays.
  The union of two arrays can be defined as the common and distinct elements in the two arrays.NOTE: Elements in the union should be in ascending order.
@@ -35,7 +36,6 @@ Distnict Elemennts in arr2 are : 11,12
 Union of arr1 and arr2 is {1,2,3,4,5,6,7,8,9,10,11,12} 
  */
 
-import java.util.*;
 
 // BRUTE FORCE
 
@@ -58,11 +58,46 @@ import java.util.*;
 
 // BETTER APPROACH
 // Two pointer Apprach
-public class FindUnion {
+import java.util.*;
+ class Union {
 
     public static void main(String[] args) {
         int[] arr1 = { 1, 2, 3, 4, 5 };
         int[] arr2 = { 2, 3, 4, 5 };
+        int n = arr1.length;
+        int m = arr2.length;
+        ArrayList<Integer> Union = new ArrayList<>();
+        int i = 0, j = 0;
+
+        while (i < n && j < m) {
+            if (arr1[i] <= arr2[j])  
+            {
+                if (Union.size() == 0 || Union.get(Union.size() - 1) != arr1[i])
+                    Union.add(arr1[i]);
+                i++;
+            } else // case 3
+            {
+                if (Union.size() == 0 || Union.get(Union.size() - 1) != arr2[j])
+                    Union.add(arr2[j]);
+                j++;
+            }
+        }
+        while (i < n) {
+            if (Union.size() == 0 || Union.get(Union.size() - 1) != arr1[i]) 
+                Union.add(arr1[i]);
+                i++;
+            
+        }
+        while (j < m) {
+            if (Union.size() == 0 || Union.get(Union.size() - 1) != arr2[j]) 
+                Union.add(arr2[j]);
+                j++;
+            
+        }
+        for(int val :Union){
+            System.out.println(val + " ");
+        }
+
 
     }
 
