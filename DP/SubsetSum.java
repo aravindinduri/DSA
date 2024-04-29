5388ed25d4381d3b477dd04bec04d7f929df2384
aravindinduri <thearavindinduri@gmail.com>
2024-04-29 16:58:53 +0530

    added minimum subset Differrence

--- DP/SubsetSum.java
+++ DP/SubsetSum.java
@@ -68,35 +68,35 @@ import java.util.*;
 
 public class SubsetSum {
     static boolean isSubsetSum(int[] a, int sum, boolean dp[][]) {
+
         int n = a.length;
         for (int i = 0; i < n; i++) {
-            for (int j = 0; j <= sum; j++) {
-                if (i == 0) {
-                    dp[i][j] = false;
-                }
-                if (j == 0) {
-                    dp[i][j] = true;
-                }
-                if(i > 0 && j > 0){
-                    boolean notTake = dp[i - 1][j];
-                    boolean take = false;
-                    if (a[i] <= j ) {
-                        take = dp[i-1][j - a[i]];
-                    }
-                    dp[i][j] = notTake || take;
-                }
+            dp[i][0] = true;
+        }
 
+        if (a[0] <= sum) {
+            dp[0][a[0]] = true;
+        }
+
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j <= sum; j++) {
+                boolean notTake = dp[i - 1][j];
+                boolean take = false;
+                if (j >= a[i]) {
+                    take = dp[i - 1][j - a[i]];
+                }
+                dp[i][j] = notTake || take;
             }
         }
-        return dp[n-1][sum];
+        return dp[n - 1][sum];
 
     }
 
     public static void main(String[] args) {
-        int[] arr = { 1,2,4,5};
+        int[] arr = { 1, 2, 3, 5 };
         int sum = 6;
-        boolean dp[][] = new boolean[arr.length+1][sum+1];
- 
+        boolean dp[][] = new boolean[arr.length][sum + 1];
+
         System.out.println(isSubsetSum(arr, sum, dp));
     }
 
