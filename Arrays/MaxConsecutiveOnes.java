c5f2f150eefb3bd09d4335614105afeb6aa8dd9e
aravindinduri <thearavindinduri@gmail.com>
2024-04-29 12:25:33 +0530

    added max consecutive one problem'

--- Arrays/MaxConsecutiveOnes.java
+++ Arrays/MaxConsecutiveOnes.java
@@ -20,8 +20,16 @@ Explanation: There are two consecutive 1's in the array.
 
 public class MaxConsecutiveOnes {
     public static void main(String[] args) {
-     int arr[] = {1,0,0,1,1,1,0,1,1,};
-     
+        int arr[] = { 1,1,1, 0, 0, 1, 0, 1, 1};
+        int count = 0, maxcount = -1;
+        for (int i = 0; i < arr.length; i++){
+            if(arr[i] == 1)
+              count++;
+            else
+              count = 0;
+            maxcount = Math.max(count,maxcount);
+        }
+        System.out.println(maxcount);
     }
-    
+
 }
